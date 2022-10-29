59e1fe278e45b04f6476bef00b5f217c2e2f577b
sergio-fogel <104943267+sergio-fogel@users.noreply.github.com>
2022-10-29 16:54:11 -0300

    Develop Data Model (#6)
    * creating a data model
    
    * filling models.py
    
    * config Alembic
    
    * config Alembic
    
    * .
    
    Closes #5

---
+++ dags/models.py
@@ -0,0 +1,21 @@
+from sqlalchemy import Column, Integer, String, DateTime, Float
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+
+class StockValue(Base):
+    """Stock value data model."""
+
+    __tablename__ = "stock_value"
+    id = Column(Integer, primary_key=True)
+    symbol = Column(String)
+    date = Column(DateTime)
+    open = Column(Float)
+    high = Column(Float)
+    low = Column(Float)
+    close = Column(Float)
+    
+    def __repr__(self):
+        return f"<StockValue(symbol='{self.symbol}', date='{self.date}', open='{self.open}', high='{self.high}', low='{self.low}', close='{self.close}')>"
+
