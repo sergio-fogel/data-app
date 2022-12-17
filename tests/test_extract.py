1a4c9a84dc462143abcd0495d54b4e724ad4301c
sergio-fogel <fogel.sergio@gmail.com>
2022-12-17 14:51:52 -0300

    .....

--- tests/test_extract.py
+++ tests/test_extract.py
@@ -7,8 +7,8 @@
 #Y validar que todo el proceso del medio corra OK
 
 
-#import sys
-#sys.path.insert(0, '/data-app')
+import sys
+sys.path.insert(0, '/data-app')
 
 import requests
 import requests_mock
